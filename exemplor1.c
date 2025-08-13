48872b76534d0738323473fdafa5a8a118688bc1
Gabriel Mello <120739225+guts7101997@users.noreply.github.com>
2025-08-13 01:12:10 +0000

    adicão de dados no exemplo

---
+++ exemplor1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e Programação II\n ");
+	printf("Ciência da Computação\n");
+	printf("1970\n");
+	
+	return 0;
+}
+
